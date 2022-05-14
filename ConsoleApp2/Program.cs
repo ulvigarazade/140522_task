f4c66a0f2fa974d6c6d60963649de96516154154
ulvigarazade <ulvigarazade@gmail.com>
2022-05-14 10:06:42 +0400

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            array[4] = 5;
+            array[5] = 6;
+            array[6] = 7;
+            array[7] = 8;
+            array[8] = 9;
+            array[9] = 10;
+
+            int a = Array.IndexOf(array, 8);
+            Console.WriteLine(a);
+        }
+    }
+}
